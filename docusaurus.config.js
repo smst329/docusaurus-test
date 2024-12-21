4cf82336eb63f9f73068064cd2b9dd3b115a108e
smst329 <68325129+smst329@users.noreply.github.com>
2024-12-21 06:53:04 +0000

    trimming out more

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,11 +65,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: 'My Docusaurus Site',
         items: [
           {to: '/blog', label: 'Blog', position: 'left'},
         ],
